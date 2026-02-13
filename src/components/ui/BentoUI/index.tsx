5e58859c27f39d6ae02c7b5646aa95864e97d3b3
Duynk <duynk198@gmail.com>
2026-02-13 23:01:04 +0700

    feat: update BentoUI to use SUB_PRODUCT_TYPE constants for widget configuration

--- src/components/ui/BentoUI/index.tsx
+++ src/components/ui/BentoUI/index.tsx
@@ -5,8 +5,11 @@ import IdentityCard from "./card/IdentityCard";
 import Personalization from "./card/Personalization";
 import RoutineCard from "./card/RoutineCard";
 import VisionBoardCard from "./card/VisionBoardCard";
-import { BENTO_UI_WIDGET_TYPES } from "@/utils/constants";
-import { useConfiguratorStore, selectOpen } from "@/stores/useConfiguratorStore";
+import { SUB_PRODUCT_TYPE } from "@/utils/constants";
+import {
+  useConfiguratorStore,
+  selectOpen,
+} from "@/stores/useConfiguratorStore";
 
 export default function BentoUI() {
   const openConfigurator = useConfiguratorStore(selectOpen);
@@ -14,19 +17,19 @@ export default function BentoUI() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-6 auto-rows-[240px]">
       <RoutineCard
-        onWidgetClick={() => openConfigurator(BENTO_UI_WIDGET_TYPES.ROUTINE)}
+        onWidgetClick={() => openConfigurator(SUB_PRODUCT_TYPE.ROUTINE)}
       />
       <CalendarCard
-        onWidgetClick={() => openConfigurator(BENTO_UI_WIDGET_TYPES.CALENDAR)}
+        onWidgetClick={() => openConfigurator(SUB_PRODUCT_TYPE.MONTH)}
       />
       <Personalization
-        onWidgetClick={() => openConfigurator(BENTO_UI_WIDGET_TYPES.PERSONALIZATION)}
+        onWidgetClick={() => openConfigurator(SUB_PRODUCT_TYPE.ADD_ON)}
       />
       <VisionBoardCard
-        onWidgetClick={() => openConfigurator(BENTO_UI_WIDGET_TYPES.VISION_BOARD)}
+        onWidgetClick={() => openConfigurator(SUB_PRODUCT_TYPE.VISION_BOARD)}
       />
       <IdentityCard
-        onWidgetClick={() => openConfigurator(BENTO_UI_WIDGET_TYPES.IDENTITY)}
+        onWidgetClick={() => openConfigurator(SUB_PRODUCT_TYPE.OWNER_INFO)}
       />
     </div>
   );
